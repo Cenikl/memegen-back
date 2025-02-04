3c4a146b6c18ab7067cb245a49dd58d3c924811b
Cenikl <99885260+Cenikl@users.noreply.github.com>
2025-02-04 17:26:50 +0300

    initial files

---
+++ src/main/java/com/project/memegen/MemegenApplication.java
@@ -0,0 +1,13 @@
+package com.project.memegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemegenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemegenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/memegen/MemegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.memegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemegenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
